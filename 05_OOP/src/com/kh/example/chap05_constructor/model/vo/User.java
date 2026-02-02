aace7203b866b3ca97cbfe8382760e98ebbc239f
dbtmd <dbtmd@YOO_>
2026-02-02 09:38:31 +0900

    update

---
+++ 05_OOP/src/com/kh/example/chap05_constructor/model/vo/User.java
@@ -0,0 +1,14 @@
+package com.kh.example.chap05_constructor.model.vo;
+
+public class User {
+	private String userId;
+	private String userName;
+	private int age;
+	private double point;
+	
+	//출력용 메소드
+	public void inform() {
+		System.out.printf("%s, %s, %d, %f\n", userId, userName, age, point);
+	}
+
+}
---
+++ 05_OOP/src/com/kh/example/chap05_constructor/run/Run.java
@@ -0,0 +1,10 @@
+package com.kh.example.chap05_constructor.run;
+
+import com.kh.example.chap05_constructor.model.vo.User;
+
+public class Run {
+	public static void main(String[] args) {
+		User u1 = new User();
+		u1.inform();
+	}
+}
