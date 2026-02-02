776c7094a59c2076658871090cf51ad2d559e39f
dbtmd <dbtmd@YOO_>
2026-02-02 10:08:07 +0900

    update

--- 05_OOP/src/com/kh/example/chap05_constructor/model/vo/User.java
+++ 05_OOP/src/com/kh/example/chap05_constructor/model/vo/User.java
@@ -10,5 +10,32 @@ public class User {
 	public void inform() {
 		System.out.printf("%s, %s, %d, %f\n", userId, userName, age, point);
 	}
+	
+	//  필드 초기화 메소드
+	public void inputValue(String id) {
+		userId = id;
+		
+	}
+	public void inputValue2(String name) {
+		userName = name;
+	}
+	
+	public void inputValue3(int nai) {
+		age = nai;
+	}
+	
+	public void inputValue4(double p) {
+		point = p;
+	}
+	
+	public void inputValue5(String id, String name, int nai, double p) {
+		userId = id;
+		userName = name;
+		age = nai;
+		point = p;
+	}
+	
+	
+	
 
 }
--- 05_OOP/src/com/kh/example/chap05_constructor/run/Run.java
+++ 05_OOP/src/com/kh/example/chap05_constructor/run/Run.java
@@ -6,5 +6,12 @@ public class Run {
 	public static void main(String[] args) {
 		User u1 = new User();
 		u1.inform();
+		u1.inputValue("user01");
+		u1.inputValue2("홍길동");
+		u1.inputValue3(28);
+		u1.inputValue4(19.99);
+		u1.inform();
+		u1.inputValue5("user02", "이순신", 30, 98.45);
+		u1.inform();
 	}
 }
